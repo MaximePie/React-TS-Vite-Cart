d4c992d9905624e1ac6b5be81bd55fcea5c1ff8b
Chatmalow <maxime.pie.mail@gmail.com>
2022-05-16 10:30:22 +0200

    feat(front): Install basic React Router config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import {
+  BrowserRouter, Routes, Route, NavLink,
+} from 'react-router-dom';
 import Chocolates from './components/pages/Chocolates/chocolates';
 import Treats from './components/pages/Treats/treats';
 import Pastries from './components/pages/Pastries/pastries';
@@ -6,14 +9,25 @@ import Total from './components/pages/Total/total';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Interface de commande</h1>
-      <p>Commandez en toute euh... simplicité. Oui, c&apos;est ça</p>
-      <Chocolates />
-      <Treats />
-      <Pastries />
-      <Total />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>Interface de commande</h1>
+        <p>Commandez en toute euh... simplicité. Oui, c&apos;est ça</p>
+        <div>
+          <NavLink to="/chocolates">Chocolats</NavLink>
+          <NavLink to="/treats">Friandises</NavLink>
+          <NavLink to="/pastries">Pâtisseries</NavLink>
+          <NavLink to="/total">Total</NavLink>
+        </div>
+        <Routes>
+          <Route path="/" element={<Chocolates />} />
+          <Route path="/chocolates" element={<Chocolates />} />
+          <Route path="/treats" element={<Treats />} />
+          <Route path="/pastries" element={<Pastries />} />
+          <Route path="/total" element={<Total />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
