f461f97fae91ab270e7d42c1a626a9c8ba9d4e05
Chatmalow <maxime.pie.mail@gmail.com>
2022-05-20 10:23:42 +0200

    feat(Redux): Persist data in store after refreshing the page

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productReducer from './productSlice';
 
+const storedReduxState = localStorage.getItem('reduxState');
+let persistedState = {};
+if (storedReduxState) {
+  persistedState = JSON.parse(storedReduxState);
+}
+
 const store = configureStore({
   reducer: {
     product: productReducer,
   },
+  preloadedState: persistedState,
+});
+
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()));
 });
 
 export type RootState = ReturnType<typeof store.getState>
